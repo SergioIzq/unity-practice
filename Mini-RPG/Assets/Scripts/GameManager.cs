a5d5d18588c9a14b1daae4dca549d8700f921816
Sergio Izquierdo Clase <seizquie@gmail.com>
2024-02-13 15:47:50 +0100

    Modificar proyecto

--- Mini-RPG/Assets/Scripts/GameManager.cs
+++ Mini-RPG/Assets/Scripts/GameManager.cs
@@ -11,6 +11,10 @@ public class GameManager : MonoBehaviour
     {
         if (Time.timeScale == 0 && Input.GetKeyDown(KeyCode.R))
         {
+            for (int i = 0; i < GameplayManager.instance.hints.Length; i++)
+            {
+                GameplayManager.instance.hints[i] = false;
+            }
             Time.timeScale = 1;
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
